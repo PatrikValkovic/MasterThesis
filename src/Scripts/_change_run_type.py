57a41b480f122c456fb431269961783127f6cffd
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2021-04-08 23:27:50 +0200

    Add script to generate PSO cpu time jobs

--- src/Scripts/_change_run_type.py
+++ src/Scripts/_change_run_type.py
@@ -9,12 +9,12 @@ import wandb
 
 api = wandb.Api()
 runs = api.runs(f'kowalsky/thesis', filters={
-    "$and": [
-        {"config.run_type": {"$gt": "2021-04-07T11:00:00"}},
+    "$or": [
+        {"config.run_type": 'testing'},
+        {"config.run_type": None},
     ]
 })
 
 for run in runs:
     run.config['run_type'] = 'test'
-    run.config['run_failed'] = True
     run.update()
---
+++ src/Scripts/generate_pso_time_cpu.py
@@ -0,0 +1,60 @@
+###############################
+#
+# Created by Patrik Valkovic
+# 08.04.21
+#
+###############################
+import argparse
+import itertools
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--output', type=str, required=True, help='Output directory')
+parser.add_argument('--cluster', type=str, default='pcr', help="Cluster where to run script")
+parser.add_argument("--cores", type=int, default=32, help="Number of real cores per machine")
+parser.add_argument('--cores_per_job', default=8, help="Number of real cores per job")
+args, _ = parser.parse_known_args()
+
+jobs_per_machine = args.cores // args.cores_per_job
+combinations = list(enumerate(itertools.product(
+    [6, 32, 128, 384],
+    [1, 7, 15, 19, 22, 24],
+)))
+
+while len(combinations) > 0:
+    with open(f'{args.output}/_job.{int(time.time())}.{combinations[0][0]}.sh', "w") as f:
+        print("#!/bin/bash", file=f)
+        print("#PBS -N PSO_CPU_time", file=f)
+        print("#PBS -l place=exclhost", file=f)
+        print("#PBS -l select=1:scratch_local=100gb:cluster=pcr", file=f)
+        print("#PBS -l walltime=24:00:00", file=f)
+        print(file=f)
+        print("cp -r /storage/praha1/home/kowalsky/PYTHON \"$SCRATCHDIR\"", file=f)
+        print("cp -r /storage/praha1/home/kowalsky/PIP \"$SCRATCHDIR\"", file=f)
+        print("cp -r /storage/praha1/home/kowalsky/FFEAT \"$SCRATCHDIR\"", file=f)
+        print("cp -r /storage/praha1/home/kowalsky/BBOBtorch \"$SCRATCHDIR\"", file=f)
+        print(file=f)
+        print("export PIP_CACHE_DIR=\"$SCRATCHDIR/pipcache\"", file=f)
+        print("export PIP_TARGET=\"$SCRATCHDIR/PIP\"", file=f)
+        print("export PYTHONPATH=\"$SCRATCHDIR/PIP\"", file=f)
+        print("export PATH=\"$SCRATCHDIR/PYTHON:$PATH\"", file=f)
+        print("export WANDB_API_KEY=\"799946cf5191b8c89fac5fcaafa346e1281ef80b\"", file=f)
+        print("export WANDB_ENTITY=\"kowalsky\"", file=f)
+        print("export WANDB_PROJECT=\"thesis\"", file=f)
+        print("export TMP=\"$SCRATCHDIR/tmp\"", file=f)
+        print("export TMPDIR=\"$SCRATCHDIR/tmp\"", file=f)
+        print("export TEMP=\"$SCRATCHDIR/tmp\"", file=f)
+        print(file=f)
+        print("cd \"$SCRATCHDIR/BBOBtorch\"", file=f)
+        print("python -m pip install --no-deps .", file=f)
+        print("cd \"$SCRATCHDIR/FFEAT\"", file=f)
+        print("python -m pip install --no-deps .", file=f)
+        print(file=f)
+        print("cp -r \"/storage/praha1/home/kowalsky/MasterThesis/src/Scripts\" \"$SCRATCHDIR\"", file=f)
+        print("cd \"$SCRATCHDIR/Scripts\"", file=f)
+        for job_id, (fdim, fn) in combinations[:jobs_per_machine]:
+            print(f"python time_pso2006_random.py --function {fn} --dim {fdim} --popsize \"32,128,200,512,1024,2048,5000,10240,16384,32768\" --device \"cpu\" --repeat 100 --cpu_count {args.cores_per_job} &", file=f)
+        print("wait", file=f)
+        print(file=f)
+        print("rm -rf \"$SCRATCHDIR\"", file=f)
+        combinations = combinations[jobs_per_machine:]
