6388f47e1d751279a46421b260defc03fd761a6c
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2021-04-09 10:57:32 +0200

    Scripts for PSO2011

---
+++ src/Scripts/generate_pso11_measure.py
@@ -0,0 +1,51 @@
+###############################
+#
+# Created by Patrik Valkovic
+# 08.04.21
+#
+###############################
+import argparse
+import itertools
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--output', type=str, help='Output directory')
+args, _ = parser.parse_known_args()
+
+for job_id, (fn, fdim) in enumerate(itertools.product(
+    [1, 7, 15, 19, 22, 24],
+    [6, 32, 128, 384]
+)):
+    with open(f'{args.output}/_job.{int(time.time())}.{job_id}.sh', "w") as f:
+        print("#!/bin/bash", file=f)
+        print("#PBS -N PSO11_GPU", file=f)
+        print("#PBS -q gpu", file=f)
+        print("#PBS -l select=1:ncpus=4:mem=8gb:scratch_local=50gb:cluster=adan:ngpus=1", file=f)
+        print("#PBS -l walltime=24:00:00", file=f)
+        print(file=f)
+        print("cp -r /storage/praha1/home/kowalsky/PYTHON \"$SCRATCHDIR\"", file=f)
+        print("cp -r /storage/praha1/home/kowalsky/PIP \"$SCRATCHDIR\"", file=f)
+        print("cp -r /storage/praha1/home/kowalsky/FFEAT \"$SCRATCHDIR\"", file=f)
+        print("cp -r /storage/praha1/home/kowalsky/BBOBtorch \"$SCRATCHDIR\"", file=f)
+        print(file=f)
+        print("export PIP_CACHE_DIR=\"$SCRATCHDIR/pipcache\"", file=f)
+        print("export PIP_TARGET=\"$SCRATCHDIR/PIP\"", file=f)
+        print("export PYTHONPATH=\"$SCRATCHDIR/PIP\"", file=f)
+        print("export PATH=\"$SCRATCHDIR/PYTHON:$PATH\"", file=f)
+        print("export WANDB_API_KEY=\"799946cf5191b8c89fac5fcaafa346e1281ef80b\"", file=f)
+        print("export WANDB_ENTITY=\"kowalsky\"", file=f)
+        print("export WANDB_PROJECT=\"thesis\"", file=f)
+        print("export TMP=\"$SCRATCHDIR/tmp\"", file=f)
+        print("export TMPDIR=\"$SCRATCHDIR/tmp\"", file=f)
+        print("export TEMP=\"$SCRATCHDIR/tmp\"", file=f)
+        print(file=f)
+        print("cd \"$SCRATCHDIR/BBOBtorch\"", file=f)
+        print("python -m pip install --no-deps .", file=f)
+        print("cd \"$SCRATCHDIR/FFEAT\"", file=f)
+        print("python -m pip install --no-deps .", file=f)
+        print(file=f)
+        print("cp -r \"/storage/praha1/home/kowalsky/MasterThesis/src/Scripts\" \"$SCRATCHDIR\"", file=f)
+        print("cd \"$SCRATCHDIR/Scripts\"", file=f)
+        print(f"python timefitness_pso2011_random.py --function {fn} --dim {fdim} --popsize \"32,512,10240,32768\" --device \"cuda\" --repeat 100 --cpu_count $(($PBS_NCPUS / 2))", file=f)
+        print(file=f)
+        print("rm -rf \"$SCRATCHDIR\"", file=f)
