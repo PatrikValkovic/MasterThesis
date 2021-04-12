6dc5750d011a8088c8c8ee5cb2b3f0c1f9d6a75b
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2021-04-12 12:03:32 +0200

    Add scripts to generate tests for PSO enighborhoods

--- src/Scripts/_change_run_type.py
+++ src/Scripts/_change_run_type.py
@@ -5,16 +5,23 @@
 #
 ###############################
 import wandb
-
+from progressbar import progressbar
 
 api = wandb.Api()
 runs = api.runs(f'kowalsky/thesis', filters={
-    "$or": [
-        {"config.run_type": 'testing'},
-        {"config.run_type": None},
+    "$and": [
+        {'config.run_type.value': 'time'},
+        {'config.device.value': 'cpu'},
+        {'config.alg_group.value': 'pso'},
+        {'config.pso.update.value': 'PSO2006'},
+        {'config.pso.neigh.value': 'Random'},
+        {'config.pop_size.value': {'$ne': None}},
+        {'createdAt': {'$lt': '2021-04-09T22:04:00'}}
     ]
 })
+runs = list(runs)
+print(f"Going to change {len(runs)} runs")
 
-for run in runs:
+for run in progressbar(runs):
     run.config['run_type'] = 'test'
     run.update()
