ac9aa9c26b6117656b0cb9d1a77aa2e49d575103
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2021-04-26 15:06:41 +0200

    Add rendering of crossovers

--- thesis/img/render_runs.py
+++ thesis/img/render_runs.py
@@ -14,7 +14,7 @@ import gzip
 import traceback
 import math
 
-CACHE_DIR = '/tmp'
+CACHE_DIR = 'D:\\runscache'
 api = wandb.Api()
 FIGSIZE=(6, 4)
 
@@ -70,8 +70,244 @@ def plot_generatelogticks(minval, maxval, ticks):
 #endregion
 
 
+#region ES crossover fitness
+#new_group('ES crossover fitness')
+NUM_Y_TICKS = 7
+POP_SIZES=[32,512,10240,32768]
+FNS= [19, 24]
+DIMS= [24, 128, 384]
+PSIZE_C = {
+    '32': 'tab:blue',
+    '512': 'tab:orange',
+    '10240': 'tab:green',
+    '32768': 'tab:red',
+}
+CROSSOVERS = [
+    {'label': 'Arithmetic','crossover': 'Arithmetic'},
+    {'label': 'Blend','crossover': 'Blend'},
+    {'label': 'One-point','crossover': 'OnePoint1D'},
+    {'label': 'Two-point','crossover': 'TwoPoint1D'},
+    {'label': 'Uniform','crossover': 'Uniform'},
+]
+STYLES=['-','--',':']
+for fn, dim, crossover in progressbar(list(itertools.product(
+    FNS,
+    DIMS,
+    CROSSOVERS,
+))):
+    plt.figure(figsize=FIGSIZE)
+    maxval = -math.inf
+    minval = math.inf
+    for psize in POP_SIZES:
+        runs = MyRun.load_and_cache({
+            "$and": [
+                {'state': 'finished'},
+                {'config.run_failed.value': False},
+                {'config.pop_size.value': psize},
+                {'config.alg_group.value': 'es_crossover'},
+                {'config.device.value': 'cuda'},
+                {'config.run_type.value': 'time,fitness'},
+                {'config.bbob_fn.value': fn},
+                {'config.bbob_dim.value': dim},
+                {'config.es.crossover.value': crossover['crossover']},
+            ]
+        }, keep_history=True)
+        print()
+        print(f"fn {fn}:{dim} for {crossover['label']}, runs: {len(runs)} for pop size {psize}")
+        print()
+        max_iter = max(map(lambda r: r.summary['iteration'], runs))
+        medians, q05, best = [], [], []
+        for step in range(max_iter+1):
+            cmedians, cq05, cbest = [], [], []
+            for r in runs:
+                h = r.scan_history()
+                if len(h) <= step:
+                    continue
+                h = h[step]
+                cmedians.append(h['fitness_median'])
+                cq05.append(h['fitness_q05'])
+                cbest.append(h['fitness_lowest'])
+            if len(cmedians) <= 20:
+                break
+            medians.append(np.mean(cmedians))
+            q05.append(np.mean(cq05))
+            best.append(np.mean(cbest))
+        minval = min(minval, np.min(best))
+        maxval = max(maxval, np.max(medians))
+        plt.plot(range(len(medians)), medians, c=PSIZE_C[str(psize)], linestyle=STYLES[0])
+        plt.plot(range(len(q05)), q05,         c=PSIZE_C[str(psize)], linestyle=STYLES[1])
+        plt.plot(range(len(best)), best,       c=PSIZE_C[str(psize)], linestyle=STYLES[2])
+    plt.yscale('log')
+    plt.gca().get_yaxis().clear()
+    plt.xlim(0, 1000)
+    plt.xlabel('Generation')
+    minval = round_plotdown(minval)
+    maxval = round_plotup(maxval)
+    plt.ylim(minval, maxval)
+    plt.yticks(plot_generatelogticks(minval, maxval, NUM_Y_TICKS))
+    plt.gca().get_yaxis().set_major_formatter(mticker.ScalarFormatter(useOffset=False))
+    plt.minorticks_off()
+    plt.ylabel('Objective function')
+    plt.title(f"{crossover['label']} crossover of $f_{{{fn}}}$ with {dim} dimensions")
+    plt.savefig(f'runs/fitness_es_crossover_f{fn}_dim{dim}_{crossover["crossover"]}.pdf')
+    plt.close()
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot()
+ax2.axis('off')
+legend = ax2.legend([
+    mlines.Line2D([0],[0], linestyle='-', c='black'),
+    mlines.Line2D([0],[0], linestyle='--', c='black'),
+    mlines.Line2D([0],[0], linestyle=':', c='black'),
+    *list(map(lambda x: mlines.Line2D([0],[0],c=x), PSIZE_C.values())),
+], [
+    'Fitness median', 'Fitness 0.05 quantile', 'Best fitness',
+    *list(map(lambda x: f"Population size {x}", PSIZE_C.keys()))
+], frameon=False, loc='lower center', ncol=10, )
+fig2 = legend.figure
+fig2.canvas.draw()
+bbox = legend.get_window_extent().transformed(fig2.dpi_scale_trans.inverted())
+fig2.savefig(
+    f"runs/fitness_es_crossovers_legend.pdf",
+    dpi="figure",
+    bbox_inches=legend.get_window_extent().transformed(fig2.dpi_scale_trans.inverted())
+)
+plt.close(fig2)
+#endregion
+
+#region ES crossover times
+#new_group('ES crossover time')
+NUM_Y_TICKS = 7
+POP_SIZES = [32,128,200,512,1024,2048,5000,10240,16384,32768]
+FNS= [19, 24]
+DIMS= [24, 128, 384]
+CROSSOVERS = [
+    {'label': 'Arithmetic','crossover': 'Arithmetic', 'c': 'tab:blue'},
+    {'label': 'Blend','crossover': 'Blend', 'c': 'tab:orange'},
+    {'label': 'One-point','crossover': 'OnePoint1D', 'c': 'tab:green'},
+    {'label': 'Two-point','crossover': 'TwoPoint1D', 'c': 'tab:red'},
+    {'label': 'Uniform','crossover': 'Uniform', 'c': 'tab:gray'},
+]
+TO_MEASURE = 'total_real_time'
+for fn, dim in progressbar(list(itertools.product(
+    FNS,
+    DIMS,
+))):
+    plt.figure(figsize=FIGSIZE)
+    maxval, minval = -math.inf, math.inf
+    for crossover in CROSSOVERS:
+        runs = MyRun.load_and_cache({
+            "$and": [
+                {'state': 'finished'},
+                {'config.run_failed.value': False},
+                {'config.pop_size.value': {'$in': POP_SIZES}},
+                {'config.alg_group.value': 'es_crossover'},
+                {'config.device.value': 'cpu'},
+                {'config.run_type.value': 'time'},
+                {'config.bbob_fn.value': fn},
+                {'config.bbob_dim.value': dim},
+                {'config.es.crossover.value': crossover['crossover']},
+            ]
+        })
+        print()
+        print(f"CROSSOVER for fn {fn}:{dim} - {crossover['label']} has {len(runs)} runs")
+        measure = np.zeros(len(POP_SIZES))
+        run_count = np.zeros(len(POP_SIZES))
+        for run in runs:
+            try:
+                s, c = run.summary, run.config
+                progress = s['iteration'] / s['max_iteration']
+                i = POP_SIZES.index(c['pop_size'])
+                measure[i] += s[TO_MEASURE] / progress
+                run_count[i] += 1
+            except:
+                traceback.print_exc()
+                print(run)
+        measure = measure[run_count > 0] / run_count[run_count > 0]
+        minval, maxval = min(measure.min(), minval), max(measure.max(), maxval)
+        plt.plot(
+            np.array(POP_SIZES)[run_count > 0],
+            measure,
+            c=crossover['c'],
+            linestyle='-',
+        )
+
+        runs = MyRun.load_and_cache({
+            "$and": [
+                {'state': 'finished'},
+                {'config.run_failed.value': False},
+                {'config.pop_size.value': {'$in': POP_SIZES}},
+                {'config.alg_group.value': 'es_crossover'},
+                {'config.device.value': 'cuda'},
+                {'config.run_type.value': 'time,fitness'},
+                {'config.bbob_fn.value': fn},
+                {'config.bbob_dim.value': dim},
+                {'config.es.crossover.value': crossover['crossover']},
+            ]
+        })
+        print()
+        print(f"CROSSOVER for fn {fn}:{dim} - {crossover['label']} has {len(runs)} runs")
+        measure = np.zeros(len(POP_SIZES))
+        run_count = np.zeros(len(POP_SIZES))
+        for run in runs:
+            try:
+                s, c = run.summary, run.config
+                progress = s['iteration'] / s['max_iteration']
+                i = POP_SIZES.index(c['pop_size'])
+                measure[i] += s[TO_MEASURE] / progress
+                run_count[i] += 1
+            except:
+                traceback.print_exc()
+                print(run)
+        measure = measure[run_count > 0] / run_count[run_count > 0]
+        minval, maxval = min(measure.min(), minval), max(measure.max(), maxval)
+        plt.plot(
+            np.array(POP_SIZES)[run_count > 0],
+            measure,
+            c=crossover['c'],
+            linestyle='--',
+        )
+    plt.title(f"BBOB function $f_{{{fn}}}$ with ${dim}$ dimensions")
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xticks([32, 128, 512, 2048, 10240, 32768])
+    plt.gca().get_xaxis().set_major_formatter(mticker.ScalarFormatter())
+    plt.gca().get_xaxis().set_tick_params(which='minor', size=0)
+    plt.xlim(32, 32768)
+    plt.xlabel('Population size')
+    minval = round_plotdown(minval)
+    maxval = round_plotup(maxval)
+    plt.ylim(minval, maxval)
+    plt.yticks(plot_generatelogticks(minval, maxval, NUM_Y_TICKS))
+    plt.gca().get_yaxis().set_major_formatter(mticker.ScalarFormatter(useOffset=False))
+    plt.minorticks_off()
+    plt.ylabel('Running time [s]')
+    plt.savefig(f"runs/time_es_crossover_fn{fn}_{dim}d.pdf")
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot()
+ax2.axis('off')
+legend = ax2.legend([
+    mlines.Line2D([0],[0], linestyle='-', c='black'),
+    mlines.Line2D([0],[0], linestyle='--', c='black'),
+    *list(map(lambda x: mlines.Line2D([0],[0],c=x['c']), CROSSOVERS)),
+], [
+    'CPU', 'CUDA',
+    *list(map(lambda x: f'{x["label"]} crossover', CROSSOVERS))
+], frameon=False, loc='lower center', ncol=10, )
+fig2 = legend.figure
+fig2.canvas.draw()
+bbox = legend.get_window_extent().transformed(fig2.dpi_scale_trans.inverted())
+fig2.savefig(
+    f"runs/time_es_crossover_legend.pdf",
+    dpi="figure",
+    bbox_inches=legend.get_window_extent().transformed(fig2.dpi_scale_trans.inverted())
+)
+plt.close(fig2)
+#endregion
+
 #region ES mutation fitness
-#new_group('ES mutation fitness')
+new_group('ES mutation fitness')
 NUM_Y_TICKS = 7
 POP_SIZES=[32, 512, 10240, 32768]
 MUTATIONS = ['AddFromNormal', 'AddFromCauchy', 'ReplaceUniform', 'AdaptiveStep']
